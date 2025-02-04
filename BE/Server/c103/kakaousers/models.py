2da54ae203124a18edeaa0ea29e221fe13b7f907
허재웅 <wkdgjwodnd@naver.com>
2025-02-04 14:18:03 +0900

    Feat: Kakao 소셜 로그인 정보 저장
    Kakao 소셜 로그인 정보 DB 생성
    
    S12P11C103-78

---
+++ BE/Server/c103/kakaousers/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class KakaoUser(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    address = models.CharField(max_length=255, null=True, blank=True, default=None)
+
+    def __str__(self):
+        return self.username
