5b2402fdaf125bc95131b9b62d1878f6bcc3a203
허재웅 <wkdgjwodnd@naver.com>
2025-02-05 14:45:41 +0900

    Fix: 불필요한 코드 삭제
    구현 되지 않은 소셜 로그인 삭제

--- BE/Client/c103/c103/settings.py
+++ BE/Client/c103/c103/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     "login",
     "client",
     "register",
-    "kakaologin",
     "robot",
 ]
 
--- BE/Client/c103/c103/urls.py
+++ BE/Client/c103/c103/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("login.urls")),  # FE는 /api/login/으로 호출
     path("api/", include("register.urls")),
-    path("api/", include("kakaologin.urls")),
     path("api/", include("robot.urls")),
 ]
