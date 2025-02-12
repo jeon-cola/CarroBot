be6a6042bf1271db688bd0d9461cf5ea15165d1e
hongseok <hghg990124@naver.com>
2025-02-12 21:39:54 +0900

    aa

--- FE/ssafy_pjt/app/src/main/java/com/example/ssafy_pjt/component/HomeSceen.kt
+++ FE/ssafy_pjt/app/src/main/java/com/example/ssafy_pjt/component/HomeSceen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -174,6 +175,7 @@ fun HomeScreen(
                                style = modeType,
                                color = colorResource(R.color.black)
                            )
+                           Spacer(modifier = Modifier.height(5.dp))
                            Icon(
                                painter = painterResource(R.drawable.footprint),
                                contentDescription = "팔로잉",
@@ -198,6 +200,7 @@ fun HomeScreen(
                                 color = colorResource(R.color.black),
                                 style = modeType
                             )
+                            Spacer(modifier = Modifier.height(5.dp))
                             Icon(
                                 painter = painterResource(R.drawable.delivery),
                                 contentDescription = "배달",
