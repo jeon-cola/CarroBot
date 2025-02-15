9a0d5d19684cd00479e8d966da1e6f6e82afa2af
윤종민 <qerter20@gmail.com>
2025-02-15 15:04:49 +0000

    Refactor: 비동기 전환을 위한 LoadScale클래스로 변환
    scale.py 코드를 LoadScale인 하나의 클래스로 변환
    전역변수를 클래스 속성으로 변경
    모든 메서드 클래스 메서드로 변경
    scale_read 메서드에서 측정된 무게 값 반환(int)
    
    S12P11C103-77

--- RP/LoadScale/scale.py
+++ RP/LoadScale/scale.py
@@ -1,76 +1,66 @@
-import time
 import sys
 import RPi.GPIO as GPIO
-from hx711 import HX711
+from .hx711 import HX711
 
-
-def cleanAndExit():
-    print("Cleaning...")
+class LoadScale:
+    def __init__(self, dt_pin, sck_pin, reference_unit=8600):
+        self.dt_pin = dt_pin
+        self.sck_pin = sck_pin
+        self.reference_unit = reference_unit
         
-    print("Scaler turned off!")
-    sys.exit()
+        # HX711 초기화
+        self.hx = HX711(dout=self.dt_pin, pd_sck=self.sck_pin)
+        self.hx.set_reading_format("MSB", "MSB")
+        self.hx.set_reference_unit(self.reference_unit)
+        
+        # 초기화 및 영점 조정
+        self.hx.reset()
+        self.hx.tare(30)
+        
+        # 드리프트 보정 변수
+        self.drift_offset = 0
+        self.drift_learning_rate = 0.001
+        
+        # 저역통과 필터 설정
+        self.alpha = 0.8
+        self.filtered_value = self.hx.get_weight(5)
+        
+        print("Tare done! Add weight now...")
 
-DT = 5
-SCK = 6
+    def update_drift(self, measured, filtered):
+        # 물체가 올려진 상태에서 무게가 거의 일정하다고 가정할 수 있다면,
+        # 측정된 값과 필터 결과의 차이를 천천히 추정 오프셋으로 반영
+        error = filtered - measured
+        self.drift_offset += self.drift_learning_rate * error
 
-hx = HX711(dout=DT, pd_sck=SCK)
-
-
-hx.set_reading_format("MSB", "MSB")
-
-
-referenceUnit = 8600
-hx.set_reference_unit(referenceUnit)
-
-hx.reset()
-
-hx.tare(30)
-
-drift_offset = 0
-drift_learning_rate = 0.001  # 드리프트 추정에 사용하는 느린 적응 속도
-
-def update_drift(measured, filtered):
-    global drift_offset
-    # 물체가 올려진 상태에서 무게가 거의 일정하다고 가정할 수 있다면,
-    # 측정된 값과 필터 결과의 차이를 천천히 추정 오프셋으로 반영
-    error = filtered - measured
-    drift_offset += drift_learning_rate * error
-
-# 저역통과 필터 설정
-# 작을수록 더 강한 필터링(느린 응답) 0~1 사이이
-alpha = 0.8  # (alpha)% 반영
-filtered_value = hx.get_weight(5)
-
-print("Tare done! Add weight now...")
-
-while True:
-    try:
-        val = hx.get_weight(5)
+    def scale_read(self) -> int:
+        val = self.hx.get_weight(5)
 
         # 드리프트 오차 보정
-        val -= drift_offset
+        val -= self.drift_offset
 
         # 저역 통과 필터
-        filtered_value = alpha * val + (1 - alpha) * filtered_value
+        self.filtered_value = self.alpha * val + (1 - self.alpha) * self.filtered_value
 
-        update_drift(val, filtered_value)
+        self.update_drift(val, self.filtered_value)
 
-        # 값 변환 및 음수 예외처리리
-        kilogram = round(filtered_value, 1)
+        # 값 변환 및 음수 예외처리
+        kilogram = round(self.filtered_value, 1)
         
         if kilogram <= 0.0:
             if kilogram > -0.5:
                 kilogram = 0.0
             else:
                 print("Waitting for Retare...")
-                hx.tare(30)
+                self.hx.tare(30)
                 print("Tare done!")
 
-        print(kilogram)
+        self.hx.power_down()
+        self.hx.power_up()
 
-        hx.power_down()
-        hx.power_up()
-        time.sleep(0.1)
+        return int(kilogram)
 
-    except (KeyboardInterrupt, SystemExit):
-        cleanAndExit()
+    def cleanAndExit(self):
+        print("Cleaning...")
+        print("Scaler turned off!")
+        sys.exit()
