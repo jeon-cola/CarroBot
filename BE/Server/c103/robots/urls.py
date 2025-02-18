d9880c0f5d7077f70b4cc9353740befad1f2af6c
허재웅 <wkdgjwodnd@naver.com>
2025-02-18 11:07:38 +0000

    Feat: 이미지 수신 구현
    이미지 수신 및 저장 구현

--- BE/Server/c103/robots/urls.py
+++ BE/Server/c103/robots/urls.py
@@ -1,8 +1,9 @@
 # login/urls.py
 from django.urls import path
-from .views import weight_view, get_gps_view
+from .views import weight_view, get_gps_view, camera_view
 
 urlpatterns = [
     path("weight/", weight_view, name="weight"),
     path("get_gps/", get_gps_view, name="get_gps"),
+    path("camera/", camera_view, name="camera"),
 ]
--- BE/Server/c103/robots/views.py
+++ BE/Server/c103/robots/views.py
@@ -62,9 +62,70 @@ def get_gps_view(request):
         "robot_id": robot_id,
     }
 
-    print("gps payload : ", payload)
-    print("latitude: ", latitude, "  longitude : ", longitude)
+    # print("gps payload : ", payload)
+    # print("latitude: ", latitude, "  longitude : ", longitude)
 
     # (2) WebSocket 서버에 "login" 패킷 전송
 
     return JsonResponse(payload)
+
+
+from datetime import datetime
+import os
+
+IMAGES_DIR = "~/media/camera_images"
+os.makedirs(IMAGES_DIR, exist_ok=True)
+
+
+@csrf_exempt
+def camera_view(request):
+    # if request.method != "POST":
+    #     return JsonResponse(
+    #         {"status": "error", "message": "Only POST method allowed."}, status=405
+    #     )
+
+    # try:
+    #     data = json.loads(request.body)
+    #     print(data)
+    # except json.JSONDecodeError:
+    #     return JsonResponse({"status": "error", "message": "Invalid JSON."}, status=400)
+
+    # frame = data.get("frame")
+    if request.method == "POST" and "frame" in request.FILES:
+        # 현재 시간을 기준으로 파일명 생성
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+        filename = f"frame_{timestamp}.jpg"
+        filepath = os.path.join(IMAGES_DIR, filename)
+
+        # 파일 저장
+        with open(filepath, "wb+") as destination:
+            for chunk in request.FILES["frame"].chunks():
+                destination.write(chunk)
+
+        # 최신 이미지 경로 업데이트
+        latest_image_path = f"/media/camera_images/{filename}"
+
+        return JsonResponse(
+            {
+                "status": "success",
+                "message": "Image received and saved",
+                "filename": filename,
+            }
+        )
+
+    return JsonResponse({"status": "error", "message": "Invalid request"}, status=400)
+
+    # TODO: footpath 가공해서 로봇에서 받을 수 있게 수정하
+
+    # payload = {
+    #     "action": "get_gps",
+    #     "latitude": latitude,
+    #     "longitude": longitude,
+    #     "robot_id": robot_id,
+    # }
+
+    print("camera_frame : ", frame)
+
+    # (2) WebSocket 서버에 "login" 패킷 전송
+
+    return JsonResponse({"status": "success"})
