b57363896e8a89cf1673f50ce87055e6fa044fc2
hongseok <hghg990124@naver.com>
2025-05-25 21:18:47 +0900

    Docs:추가

---
+++ src/FE/ssafy_pjt/app/build/generated/source/buildConfig/debug/com/example/ssafy_pjt/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.ssafy_pjt;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.ssafy_pjt";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String GOOGLE_OAUTH_CLIENT_ID = "null";
+  // Field from default config.
+  public static final String KAKAO_NATIVE_APP_KEY = "6c9bbea7e870ab67fd9b8a564b30079f";
+  // Field from default config.
+  public static final String SK_app_key = "z5zMc6Ul672e90UMeb5RQ7aA8MLG9zTZ8zQwBWEN";
+}
